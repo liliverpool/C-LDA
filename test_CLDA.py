4126bc0eb342294c8b597ca81cc5abc943be04af
liliverpool <344733705@qq.com>
2019-04-12 21:54:05 +0800

    Update test_CLDA.py

--- test_CLDA.py
+++ test_CLDA.py
@@ -29,12 +29,12 @@ stop_list = readtext.split('\n')
 texts = [[word for word in line.lower().split() if word not in stop_list] for line in tdt2_data] 
 t_data = texts[:clip]
 
-save_p = "Contextual_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+str(pbeta)+"_"+str(pgamma)+"\\"
+save_p = "Contextual_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+str(pbeta)+"_"+str(pgamma)+r"\"
 # RUN C-LDA
 C_LDA.run(t_data, start, end, iteration_num, save_p, clip, c_len, palpha, pbeta, pgamma)
 
 dataset = save_p
-y1 = np.load(str(dataset) +"\\C-LDAper_list"+ str(topics) +".npy")
+y1 = np.load(str(dataset) +"C-LDAper_list"+ str(topics) +".npy")
 x = np.linspace(0, iteration_num, iteration_num)
 plt.plot(x[::1], y1[:], "r*-", label='C-LDA', linewidth=1)
 plt.title("Convergence Test By Perplexities")
@@ -43,11 +43,11 @@ plt.xlabel(u"Iterations")
 plt.legend(loc="upper right")
 plt.show()
 
-save_p2 = "tradition_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+str(pbeta)+"_"+str(pgamma)+"\\"
+save_p2 = "tradition_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+str(pbeta)+"_"+str(pgamma)+r"\"
 t_LDA.run(t_data, start, end, iteration_num, save_p2, clip, c_len, palpha, pbeta, pgamma)
 
 dataset = save_p2
-y2 = np.load(str(dataset) +"\\LDAper_list"+ str(topics) +".npy")
+y2 = np.load(str(dataset) +"LDAper_list"+ str(topics) +".npy")
 x = np.linspace(0, iteration_num, iteration_num)
 plt.plot(x[::1], y1[:], "r*-", label='C-LDA', linewidth=1)
 plt.plot(x[::1], y2[:], "b+-", label='LDA', linewidth=1)
