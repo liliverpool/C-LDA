49e5fe713db8702d1fb88d270f075a6e222ccc89
liliverpool <344733705@qq.com>
2019-10-11 14:04:05 +0900

    Update test_CLDA.py

--- test_CLDA.py
+++ test_CLDA.py
@@ -21,7 +21,7 @@ pbeta = 0.05
 c_len = 10
 
 # TDT2 Dataset
-tdt2_data = np.load("tdt2_em_v4_0_100.npy")
+tdt2_data = np.load("tdt2_em_v4_0_100.npy",allow_pickle=True)
 tdt2_data = tdt2_data[:clip]
 stop_file = open('stopwords2.txt', 'r')
 readtext = stop_file.read()
@@ -34,7 +34,7 @@ save_p = "Contextual_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+st
 C_LDA.run(t_data, start, end, iteration_num, save_p, clip, c_len, palpha, pbeta, pgamma)
 
 dataset = save_p
-y1 = np.load(str(dataset) +"C-LDAper_list"+ str(topics) +".npy")
+y1 = np.load(str(dataset) +"C-LDAper_list"+ str(topics) +".npy",allow_pickle=True)
 x = np.linspace(0, iteration_num, iteration_num)
 plt.plot(x[::1], y1[:], "r*-", label='C-LDA', linewidth=1)
 plt.title("Convergence Test By Perplexities")
@@ -47,7 +47,7 @@ save_p2 = "tradition_LDA_EXP" + str(clip)+"_" +str(c_len)+"_"+str(palpha)+"_"+st
 t_LDA.run(t_data, start, end, iteration_num, save_p2, clip, c_len, palpha, pbeta, pgamma)
 
 dataset = save_p2
-y2 = np.load(str(dataset) +"LDAper_list"+ str(topics) +".npy")
+y2 = np.load(str(dataset) +"LDAper_list"+ str(topics) +".npy",allow_pickle=True)
 x = np.linspace(0, iteration_num, iteration_num)
 plt.plot(x[::1], y1[:], "r*-", label='C-LDA', linewidth=1)
 plt.plot(x[::1], y2[:], "b+-", label='LDA', linewidth=1)
